b59fa88c040e2bf2651878a974b65adf934d9ab7
hara_osamu@comture.com <hara_osamu@comture.com>
2022-03-16 05:51:57 +0000

    addcol

---
+++ Libraries/Python/dbxdemo/addcol.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+import pyspark.sql.functions as F
+
+def with_status(df):
+    return df.withColumn("status", F.lit("checked"))
+
+# COMMAND ----------
+
+
---
+++ Libraries/Python/dbxdemo/spark.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def get_spark():
+    return (SparkSession.builder
+                .master("local")
+                .appName("gill")
+                .getOrCreate())
+
+# COMMAND ----------
+
+
