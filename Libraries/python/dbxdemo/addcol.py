365274ace2f9a26386b4c05321835560c593e6d5
hara_osamu@comture.com <hara_osamu@comture.com>
2022-03-17 01:20:03 +0000

    setup.py

--- Libraries/python/dbxdemo/addcol.py
+++ Libraries/python/dbxdemo/addcol.py
@@ -3,7 +3,3 @@ import pyspark.sql.functions as F
 
 def with_status(df):
     return df.withColumn("status", F.lit("checked"))
-
-# COMMAND ----------
-
-
---
+++ Libraries/python/dbxdemo/setup.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+from setuptools import setup, find_packages
+
+setup(
+    name='addcol',
+    version='1.0',
+    license='proprietary',
+    description='addcol',
+
+    author='hara_osamu',
+    author_email='hara_osamu@comture.com',
+    url='None.com',
+
+    packages=find_packages(),
+)
+
